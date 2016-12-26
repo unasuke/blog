a8ddbd5c553e82bf07518e2a8539f7b0c342ab54
Yusuke Nakamura <yusuke1994525@gmail.com>
2016-12-27 06:29:28 +0900

    Ignore asset hash

--- config.rb
+++ config.rb
@@ -68,10 +68,7 @@ configure :build do
   activate :minify_javascript
 
   # Enable cache buster
-  activate :asset_hash do |opt|
-    opt.ignore += 'images/ogp_image.jpg'
-    opt.ignore += 'images/favicon.png'
-  end
+  activate :asset_hash, ignore: %r[images/(ogp_image\.jpg|favicon\.png)]
 
   # Use relative URLs
   #activate :relative_assets
