724f515914631e58a5212af4f989da81b78d01a7
Yusuke Nakamura <yusuke1994525@gmail.com>
2022-08-28 14:48:23 +0900

    Fix generate_css for dart-sass

--- bin/generate_css.bash
+++ bin/generate_css.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-NODE_SASS_OPTIONS="--output-style=expanded"
+NODE_SASS_OPTIONS="--style=expanded --no-source-map"
 
 for file in `ls -1 source/stylesheets/src/[!_]*.scss`; do
-  node-sass $file $NODE_SASS_OPTIONS -o source/stylesheets/;
+  sass $NODE_SASS_OPTIONS $file source/stylesheets/all.css;
 done
